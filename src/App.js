8966f026e56ab0ffa56dc95699f0d5b6195b75e6
Anmol376OP <anmolsrivastav7102@gmail.com>
2023-10-08 09:03:05 +0530

    setup routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Login from './pages/login';
+import Home from './pages/home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App w-full h-full">
-      <Login />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
