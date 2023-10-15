d3501e6c0f706b33bf10b01fbaf5a143cad8919a
Anmol376OP <anmolsrivastav7102@gmail.com>
2023-10-15 17:10:21 +0530

    Initial layout built

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import '../styles/navbar.css'
 
-function Navbar() {
+function Navbar(props) {
     return (
-        <div className='w-full h-[30px] bg-[#101010]'>
-
+        <div className='w-full h-[60px] flex items-center justify-between px-4'>
+            <div className='text-3xl'>{props.title}</div>
+            <div className='flex gap-2'>
+                <div className='relative button-icon z-[200] flex flex-col border w-[40px] h-[40px] rounded-[10px]' onClick={() => props.setActive(!props.active)}>
+                    <div className={props.active ? 'bar horizontal active' : 'bar horizontal'}></div>
+                    <div className={props.active ? 'bar vertical active' : 'bar vertical'}></div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,38 @@
 import React, { useState } from 'react'
+import Navbar from '../components/navbar';
 import '../styles/home.css'
 import bgimg from '../assets/moneyplant.jpg'
+import LogoutIcon from '@mui/icons-material/Logout';
+import Dashboard from '../components/dashboard'
+import Edit from '../components/edit'
+import ViewHistory from '../components/viewhistory'
+import ContactUs from '../components/contact_us'
 
 function Home() {
     const col = '1f1f27';
     const [active, setActive] = useState(false)
+    const [activeIndex, setActiveIndex] = useState(1)
+    const NavData = [
+        {
+            index: 1,
+            title: 'Dashboard'
+        },
+        {
+            index: 2,
+            title: 'Edit Activity'
+        },
+        {
+            index: 3,
+            title: 'View History'
+        },
+        {
+            index: 4,
+            title: 'Help & Support'
+        },
+    ]
     return (
-        <div className='relative w-[100vw] h-[100vh] bg-[#141518] text-white flex home-body'>
-            <button className='z-[200] p-2 bg-red-300' onClick={() => setActive(!active)}>Click Me</button>
+        <div className='relative w-[100vw] h-[100vh] bg-[#141518] text-white flex flex-col home-body'>
+            <Navbar active={active} setActive={setActive} index={activeIndex} title={NavData[activeIndex - 1].title} />
             <div className={active ? 'sidebar-right active' : 'sidebar-right'}></div>
             <div className={active ? 'sidebar-left-upper active' : 'sidebar-left-upper'}>
                 <div className='bg-gray-400 w-[50vw] h-[60vh] z-[100]'>
@@ -15,9 +40,18 @@ function Home() {
                 </div>
             </div>
             <div className={active ? 'sidebar-lower-left active' : 'sidebar-lower-left'}>
-                <div className='text-[70px] font-bold pl-4'>Budget Beacon</div>
+                <div className='text-[70px] font-bold pl-8'>Budget Beacon</div>
             </div>
-            <div className={active ? 'sidebar-lower-right active' : 'sidebar-lower-right'}></div>
+            <div className={active ? 'sidebar-lower-right active' : 'sidebar-lower-right'}>
+                <div className='w-full h-full flex flex-col items-center justify-center cursor-pointer'>
+                    <LogoutIcon fontSize='large' />
+                    <p className='font-bold text-[45px]'> Logout</p>
+                </div>
+            </div>
+            {activeIndex === 1 && <Dashboard />}
+            {activeIndex === 2 && <Edit />}
+            {activeIndex === 3 && <ViewHistory />}
+            {activeIndex === 4 && <ContactUs />}
         </div>
     )
 }
