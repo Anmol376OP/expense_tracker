8946382dc59760bb30aaa7bcc4a66b862be8cdff
Anmol376OP <anmolsrivastav7102@gmail.com>
2023-10-29 11:01:02 +0530

    Added valid checks to values

--- src/components/edit.js
+++ src/components/edit.js
@@ -12,6 +12,7 @@ function Edit() {
     const [type, setType] = useState('')
     const [date, setDate] = useState('')
     const [category, setCategory] = useState('')
+    const [errorMsg, setErrorMsg] = useState('')
     const ExpenseType = [
         { value: 'Utility', index: '1' },
         { value: 'Healthcare', index: '2' },
@@ -28,19 +29,52 @@ function Edit() {
             }
         }
     }
+
+    const HandleClick = (amt, description, type, category, date) => {
+        if (amt === '' || description === '' || type === '' || category === '' || date === '') {
+            setErrorMsg('All feilds must be filled')
+            return
+        }
+
+        if (!(!isNaN(amt) && isFinite(amt))) {
+            setErrorMsg('Not a valid number')
+        }
+
+        const dateRegex = /^\d{2}\/\d{2}\/\d{4}$/;
+        if (!dateRegex.test(date)) {
+            setErrorMsg('Invalid date format, use DD/MM/YYYY')
+            return
+        }
+        const [day, month, year] = date.split('/').map(Number);
+        const newdate = new Date(year, month - 1, day);
+        let currDate = new Date();
+        let isProperDate = day >= 1 &&
+            day <= (month === 2 ? (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0) ? 29 : 28) : [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month - 1]) &&
+            month >= 1 &&
+            month <= 12 &&
+            newdate.getFullYear() === year &&
+            newdate.getMonth() === month - 1 &&
+            newdate.getDate() === day
+
+        if (!isProperDate) {
+            setErrorMsg('Not a valid date')
+            return
+        }
+        if (newdate > currDate) {
+            setErrorMsg(`Pls adjust your time machine, today\'s date is ${currDate}`)
+            return
+        }
+        setErrorMsg('')
+        return
+    }
     return (
         <div className='w-full h-full flex flex-col py-8 px-4 items-center'>
-            <div className='bg-[#1f1f27] flex flex-col w-full max-w-[500px] h-fit gap-8 px-4 py-8 items-center rounded-lg'>
+            <div className='bg-[#1f1f27] flex flex-col w-full max-w-[500px] h-fit gap-4 px-4 py-8 items-center rounded-lg'>
                 <div className='flex relative w-full h-fit justify-between px-[7.5%] items-center'>
                     <div className='text-2xl font-semibold text-gray-500 '>Activity Details</div>
                     <div className='mt-[5px]' onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}><InfoIcon /></div>
                     {hover && <div className='absolute top-[35px] right-[0] bg-white text-black p-2 z-[100] flex flex-col'>
-                        Type takes these values based on reason behind transaction
-                        <span>1-Utility</span>
-                        <span>2-HealthCare</span>
-                        <span>3-Essentails </span>
-                        <span>4-Entertainment</span>
-                        <span>5-Miscellaneous</span>
+                        Enter all the details so we can categorise and display accordingly
                     </div>}
                 </div>
                 <div className='w-full h-full flex flex-col gap-6 items-center text-white'>
@@ -65,8 +99,8 @@ function Edit() {
                     </Select>
 
                     <Select displayEmpty MenuProps={MenuProps} sx={{
-                        width: '85%', border: '1px solid grey', '& .MuiSelect-icon': {
-                            fill: 'grey'
+                        width: '85%', border: '1px solid grey', '& .MuiSelect-icon': { fill: 'grey' }, '&:focus': {
+                            border: '1px solid white',
                         }
                     }} renderValue={() => {
                         return category ? <span className='text-white'>{category}</span> : <span className='text-white'>Category</span>
@@ -84,8 +118,9 @@ function Edit() {
                     <CssTextField id="outlined-basic" label="Date (DD/MM/YYYY)" variant="outlined" sx={{ input: { color: 'white' }, width: '85%' }} value={date} onChange={(e) => { setDate(e.target.value) }} />
 
                 </div>
+                {errorMsg && <div className='text-red-700 px-[7.5%] text-left'>{errorMsg}</div>}
                 <div className=''>
-                    <ColorButton variant="outlined" color='error'>Add Activity</ColorButton>
+                    <ColorButton variant="outlined" color='error' onClick={() => { HandleClick(amount, desc, type, category, date) }}>Add Activity</ColorButton>
                 </div>
             </div>
 
